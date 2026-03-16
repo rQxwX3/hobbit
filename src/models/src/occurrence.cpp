111222610fefdd43f7094f70669a5d16517ee4a5
rQxwX3 <github@scrt.simplelogin.com>
2026-03-16 18:51:14 +0300

    Implement toJSON() and fromJSON() for mods::Occurrence

--- src/models/src/occurrence.cpp
+++ src/models/src/occurrence.cpp
@@ -9,19 +9,18 @@ Occurrence::Occurrence(hbt::mods::Date date) : date_{date} {}
     return date_.getWeekday();
 }
 
-// TODO implement date.toJSON()
-// [[nodiscard]] auto Occurrence::toJSON() const -> nlohmann::json {
-//     return {{"date", date_.toJSON()}};
-// }
+[[nodiscard]] auto Occurrence::toJSON() const -> nlohmann::json {
+    return {{"date", date_.toJSON()}};
+}
 
-// [[nodiscard]] auto Occurrence::fromJSON(const nlohmann::json &json)
-//     -> Occurrence {
-//     if (!json.contains("date")) {
-//         throw std::runtime_error("Missing required fields");
-//     }
-//
-//     return {json[date]};
-// }
+[[nodiscard]] auto Occurrence::fromJSON(const nlohmann::json &json)
+    -> Occurrence {
+    if (!json.contains("date")) {
+        throw std::runtime_error("Missing required fields");
+    }
+
+    return Occurrence{hbt::mods::Date::fromJSON(json["date"])};
+}
 
 [[nodiscard]] auto Occurrence::getDate() const -> hbt::mods::Date {
     return date_;
