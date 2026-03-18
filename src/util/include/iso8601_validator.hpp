16842da3ddd3fa9025b31d86ecd751c1b7e0f136
rQxwX3 <github@scrt.simplelogin.com>
2026-03-18 21:26:30 +0300

    Implement regex-based util::ISO8601Validator

--- src/util/include/iso8601_validator.hpp
+++ src/util/include/iso8601_validator.hpp
@@ -4,21 +4,43 @@
 
 namespace hbt::util {
 class ISO8601Validator {
-  public:
+  private:
     enum class ErrorCode : uint8_t {
+        NONE,
+
         EMPTY_STRING,
         MISSING_P_PREFIX,
-        MISSING_T_PREFIX,
-        DESIGNATOR_WITHOUT_NUMBER,
-        NUMBER_WITHOUT_DESIGNATOR
+
+        REGEX_MISMATCH,
     };
 
-    struct Result {
-        bool valid;
-        std::string error;
-        size_t errorPosition;
+    class Result {
+      private:
+        bool valid_;
+        std::string errorMessage_;
+        size_t errorPosition_;
 
-        explicit operator bool() const { return valid; }
+      private:
+        Result(bool valid, ISO8601Validator::ErrorCode errorCode,
+               size_t errorPosition)
+            : valid_{valid},
+
+              errorMessage_{
+                  ISO8601Validator::errorCodeToDescription(errorCode)},
+
+              errorPosition_{errorPosition} {}
+
+      public:
+        static auto success() -> Result {
+            return Result{true, ErrorCode::NONE, std::string::npos};
+        }
+
+        static auto failure(ErrorCode errorCode, size_t errorPosition)
+            -> Result {
+            return Result{false, errorCode, errorPosition};
+        }
+
+        explicit operator bool() const { return valid_; }
     };
 
   public:
@@ -28,13 +50,11 @@ class ISO8601Validator {
     [[nodiscard]] auto static validate(const std::string &string) -> Result;
 
   private:
+    [[nodiscard]] static auto isNotEmpty(const std::string &string) -> Result;
+
     [[nodiscard]] static auto startsWithP(const std::string &string) -> Result;
 
-    [[nodiscard]] static auto validTimeSection(const std::string &string)
-        -> Result;
-
-    [[nodiscard]] static auto
-    designatorsFollowedByNumbers(const std::string &string) -> Result;
+    [[nodiscard]] static auto matchesRegex(const std::string &string) -> Result;
 
   private:
     [[nodiscard]] static auto errorCodeToDescription(ErrorCode errorCode)
