24fc78a111ecd112a3e0612c0380c5afd3d2f789
rQxwX3 <github@scrt.simplelogin.com>
2026-03-24 14:16:30 +0300

    Add ctor tests for mods::DurationUnits

--- tests/unit/models/test_duration_units.cpp
+++ tests/unit/models/test_duration_units.cpp
@@ -3,8 +3,10 @@
 #include <duration_units.hpp>
 
 namespace test::mods {
+using hbt::mods::DurationUnits;
+
 TEST(DurationUnitsTest, EmptyOnConstruction) {
-    auto durationUnits{hbt::mods::DurationUnits{}};
+    auto durationUnits{DurationUnits{}};
 
     ASSERT_EQ(durationUnits.getYears(), 0);
     ASSERT_EQ(durationUnits.getMonths(), 0);
@@ -14,8 +16,35 @@ TEST(DurationUnitsTest, EmptyOnConstruction) {
     ASSERT_EQ(durationUnits.getMinutes(), 0);
 }
 
+TEST(DurationUnitsTest, ArrayConstructor) {
+    auto durationUnits{DurationUnits{DurationUnits::array_t{1, 2, 3, 4, 5, 6}}};
+
+    ASSERT_EQ(durationUnits.getYears(), 1);
+    ASSERT_EQ(durationUnits.getMonths(), 2);
+    ASSERT_EQ(durationUnits.getWeeks(), 3);
+    ASSERT_EQ(durationUnits.getDays(), 4);
+    ASSERT_EQ(durationUnits.getHours(), 5);
+    ASSERT_EQ(durationUnits.getMinutes(), 6);
+}
+
+TEST(DurationUnitsTest, PerUnitConstructor) {
+    auto durationUnits{DurationUnits{{.years = 1,
+                                      .months = 2,
+                                      .weeks = 3,
+                                      .days = 4,
+                                      .hours = 5,
+                                      .minutes = 6}}};
+
+    ASSERT_EQ(durationUnits.getYears(), 1);
+    ASSERT_EQ(durationUnits.getMonths(), 2);
+    ASSERT_EQ(durationUnits.getWeeks(), 3);
+    ASSERT_EQ(durationUnits.getDays(), 4);
+    ASSERT_EQ(durationUnits.getHours(), 5);
+    ASSERT_EQ(durationUnits.getMinutes(), 6);
+}
+
 TEST(DurationUnitsTest, IsZero) {
-    auto durationUnits{hbt::mods::DurationUnits{}};
+    auto durationUnits{DurationUnits{}};
     EXPECT_TRUE(durationUnits.isZero());
 
     durationUnits.addDays(1);
@@ -23,7 +52,7 @@ TEST(DurationUnitsTest, IsZero) {
 }
 
 TEST(DurationUnitsTest, AddGetUnits) {
-    auto durationUnits{hbt::mods::DurationUnits{}};
+    auto durationUnits{DurationUnits{}};
 
     durationUnits.addYears(1);
     EXPECT_EQ(durationUnits.getYears(), 1);
@@ -45,84 +74,84 @@ TEST(DurationUnitsTest, AddGetUnits) {
 }
 
 TEST(DurationUnitsTest, AddAutoChangesUnits) {
-    auto monthsToYears1{hbt::mods::DurationUnits{}};
-    monthsToYears1.addMonths(hbt::mods::DurationUnits::monthsInYear);
+    auto monthsToYears1{DurationUnits{}};
+    monthsToYears1.addMonths(DurationUnits::monthsInYear);
     EXPECT_EQ(monthsToYears1.getMonths(), 0);
     EXPECT_EQ(monthsToYears1.getYears(), 1);
 
-    auto monthsToYears2{hbt::mods::DurationUnits{}};
-    monthsToYears2.addMonths(hbt::mods::DurationUnits::monthsInYear * 2);
+    auto monthsToYears2{DurationUnits{}};
+    monthsToYears2.addMonths(DurationUnits::monthsInYear * 2);
     EXPECT_EQ(monthsToYears2.getMonths(), 0);
     EXPECT_EQ(monthsToYears2.getYears(), 2);
 
-    auto monthsToYears3{hbt::mods::DurationUnits{}};
-    monthsToYears3.addMonths(hbt::mods::DurationUnits::monthsInYear + 1);
+    auto monthsToYears3{DurationUnits{}};
+    monthsToYears3.addMonths(DurationUnits::monthsInYear + 1);
     EXPECT_EQ(monthsToYears3.getMonths(), 1);
     EXPECT_EQ(monthsToYears3.getYears(), 1);
 
-    auto weeksToMonths1{hbt::mods::DurationUnits{}};
-    weeksToMonths1.addWeeks(hbt::mods::DurationUnits::weeksInMonth);
+    auto weeksToMonths1{DurationUnits{}};
+    weeksToMonths1.addWeeks(DurationUnits::weeksInMonth);
     EXPECT_EQ(weeksToMonths1.getWeeks(), 0);
     EXPECT_EQ(weeksToMonths1.getMonths(), 1);
 
-    auto weeksToMonths2{hbt::mods::DurationUnits{}};
-    weeksToMonths2.addWeeks(hbt::mods::DurationUnits::weeksInMonth * 3);
+    auto weeksToMonths2{DurationUnits{}};
+    weeksToMonths2.addWeeks(DurationUnits::weeksInMonth * 3);
     EXPECT_EQ(weeksToMonths2.getWeeks(), 0);
     EXPECT_EQ(weeksToMonths2.getMonths(), 3);
 
-    auto weeksToMonths3{hbt::mods::DurationUnits{}};
-    weeksToMonths3.addWeeks(hbt::mods::DurationUnits::weeksInMonth + 2);
+    auto weeksToMonths3{DurationUnits{}};
+    weeksToMonths3.addWeeks(DurationUnits::weeksInMonth + 2);
     EXPECT_EQ(weeksToMonths3.getWeeks(), 2);
     EXPECT_EQ(weeksToMonths3.getMonths(), 1);
 
-    auto daysToWeek1{hbt::mods::DurationUnits{}};
-    daysToWeek1.addDays(hbt::mods::DurationUnits::daysInWeek);
+    auto daysToWeek1{DurationUnits{}};
+    daysToWeek1.addDays(DurationUnits::daysInWeek);
     EXPECT_EQ(daysToWeek1.getDays(), 0);
     EXPECT_EQ(daysToWeek1.getWeeks(), 1);
 
-    auto daysToWeek2{hbt::mods::DurationUnits{}};
-    daysToWeek2.addDays(hbt::mods::DurationUnits::daysInWeek * 3);
+    auto daysToWeek2{DurationUnits{}};
+    daysToWeek2.addDays(DurationUnits::daysInWeek * 3);
     EXPECT_EQ(daysToWeek2.getDays(), 0);
     EXPECT_EQ(daysToWeek2.getWeeks(), 3);
 
-    auto daysToWeek3{hbt::mods::DurationUnits{}};
-    daysToWeek3.addDays(hbt::mods::DurationUnits::daysInWeek + 4);
+    auto daysToWeek3{DurationUnits{}};
+    daysToWeek3.addDays(DurationUnits::daysInWeek + 4);
     EXPECT_EQ(daysToWeek3.getDays(), 4);
     EXPECT_EQ(daysToWeek3.getWeeks(), 1);
 
-    auto hoursToDays1{hbt::mods::DurationUnits{}};
-    hoursToDays1.addHours(hbt::mods::DurationUnits::hoursInDay);
+    auto hoursToDays1{DurationUnits{}};
+    hoursToDays1.addHours(DurationUnits::hoursInDay);
     EXPECT_EQ(hoursToDays1.getHours(), 0);
     EXPECT_EQ(hoursToDays1.getDays(), 1);
 
-    auto hoursToDays2{hbt::mods::DurationUnits{}};
-    hoursToDays2.addHours(hbt::mods::DurationUnits::hoursInDay * 5);
+    auto hoursToDays2{DurationUnits{}};
+    hoursToDays2.addHours(DurationUnits::hoursInDay * 5);
     EXPECT_EQ(hoursToDays2.getHours(), 0);
     EXPECT_EQ(hoursToDays2.getDays(), 5);
 
-    auto hoursToDays3{hbt::mods::DurationUnits{}};
-    hoursToDays3.addHours(hbt::mods::DurationUnits::hoursInDay + 15);
+    auto hoursToDays3{DurationUnits{}};
+    hoursToDays3.addHours(DurationUnits::hoursInDay + 15);
     EXPECT_EQ(hoursToDays3.getHours(), 15);
     EXPECT_EQ(hoursToDays3.getDays(), 1);
 
-    auto minutesToHours1{hbt::mods::DurationUnits{}};
-    minutesToHours1.addMinutes(hbt::mods::DurationUnits::minutesInHour);
+    auto minutesToHours1{DurationUnits{}};
+    minutesToHours1.addMinutes(DurationUnits::minutesInHour);
     EXPECT_EQ(minutesToHours1.getMinutes(), 0);
     EXPECT_EQ(minutesToHours1.getHours(), 1);
 
-    auto minutesToHours2{hbt::mods::DurationUnits{}};
-    minutesToHours2.addMinutes(hbt::mods::DurationUnits::minutesInHour * 6);
+    auto minutesToHours2{DurationUnits{}};
+    minutesToHours2.addMinutes(DurationUnits::minutesInHour * 6);
     EXPECT_EQ(minutesToHours2.getMinutes(), 0);
     EXPECT_EQ(minutesToHours2.getHours(), 6);
 
-    auto minutesToHours3{hbt::mods::DurationUnits{}};
-    minutesToHours3.addMinutes(hbt::mods::DurationUnits::minutesInHour + 30);
+    auto minutesToHours3{DurationUnits{}};
+    minutesToHours3.addMinutes(DurationUnits::minutesInHour + 30);
     EXPECT_EQ(minutesToHours3.getMinutes(), 30);
     EXPECT_EQ(minutesToHours3.getHours(), 1);
 }
 
 TEST(DurationUnitsTest, OperatorPlus) {
-    auto first{hbt::mods::DurationUnits{}};
+    auto first{DurationUnits{}};
     first.addYears(1);
     first.addMonths(1);
     first.addWeeks(1);
@@ -130,7 +159,7 @@ TEST(DurationUnitsTest, OperatorPlus) {
     first.addHours(1);
     first.addMinutes(1);
 
-    auto second{hbt::mods::DurationUnits{}};
+    auto second{DurationUnits{}};
     second.addYears(1);
     second.addMonths(2);
     second.addWeeks(3);
@@ -148,8 +177,8 @@ TEST(DurationUnitsTest, OperatorPlus) {
 }
 
 TEST(DurationUnitsTest, ComparisonOperators) {
-    auto first{hbt::mods::DurationUnits{}};
-    auto second{hbt::mods::DurationUnits{}};
+    auto first{DurationUnits{}};
+    auto second{DurationUnits{}};
     EXPECT_TRUE(first == second);
     EXPECT_TRUE(second == first);
 
@@ -172,107 +201,107 @@ TEST(DurationUnitsTest, ComparisonOperators) {
 }
 
 TEST(DurationUnitsTest, ToFromValidISO8601String) {
-    auto first{hbt::mods::DurationUnits{}};
+    auto first{DurationUnits{}};
     auto firstISO{first.toISO8601String()};
     EXPECT_EQ(firstISO, "PT0M");
-    auto restored{hbt::mods::DurationUnits::fromISO8601String(firstISO)};
+    auto restored{DurationUnits::fromISO8601String(firstISO)};
     ASSERT_TRUE(restored.has_value());
     EXPECT_EQ(restored.value(), first);
 
     first.addMinutes(1);
     firstISO = first.toISO8601String();
     EXPECT_EQ(firstISO, "PT1M");
-    restored = hbt::mods::DurationUnits::fromISO8601String(firstISO);
+    restored = DurationUnits::fromISO8601String(firstISO);
     ASSERT_TRUE(restored.has_value());
     EXPECT_EQ(restored.value(), first);
 
     first.addHours(1);
     firstISO = first.toISO8601String();
     EXPECT_EQ(firstISO, "PT1H1M");
-    restored = hbt::mods::DurationUnits::fromISO8601String(firstISO);
+    restored = DurationUnits::fromISO8601String(firstISO);
     ASSERT_TRUE(restored.has_value());
     EXPECT_EQ(restored.value(), first);
 
     first.addDays(1);
     firstISO = first.toISO8601String();
     EXPECT_EQ(firstISO, "P1DT1H1M");
-    restored = hbt::mods::DurationUnits::fromISO8601String(firstISO);
+    restored = DurationUnits::fromISO8601String(firstISO);
     ASSERT_TRUE(restored.has_value());
     EXPECT_EQ(restored.value(), first);
 
     first.addWeeks(1);
     firstISO = first.toISO8601String();
     EXPECT_EQ(firstISO, "P1W1DT1H1M");
-    restored = hbt::mods::DurationUnits::fromISO8601String(firstISO);
+    restored = DurationUnits::fromISO8601String(firstISO);
     ASSERT_TRUE(restored.has_value());
     EXPECT_EQ(restored.value(), first);
 
     first.addMonths(1);
     firstISO = first.toISO8601String();
     EXPECT_EQ(firstISO, "P1M1W1DT1H1M");
-    restored = hbt::mods::DurationUnits::fromISO8601String(firstISO);
+    restored = DurationUnits::fromISO8601String(firstISO);
     ASSERT_TRUE(restored.has_value());
     EXPECT_EQ(restored.value(), first);
 
     first.addYears(1);
     firstISO = first.toISO8601String();
     EXPECT_EQ(firstISO, "P1Y1M1W1DT1H1M");
-    restored = hbt::mods::DurationUnits::fromISO8601String(firstISO);
+    restored = DurationUnits::fromISO8601String(firstISO);
     ASSERT_TRUE(restored.has_value());
     EXPECT_EQ(restored.value(), first);
 }
 
 TEST(DurationUnitsTest, ToFromInValidISO8601String) {
     // invalid empty duration
-    auto invalidISO{hbt::mods::DurationUnits::fromISO8601String("PT")};
+    auto invalidISO{DurationUnits::fromISO8601String("PT")};
     ASSERT_TRUE(!invalidISO.has_value());
 
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("");
+    invalidISO = DurationUnits::fromISO8601String("");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // no global prefix
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("1Y");
+    invalidISO = DurationUnits::fromISO8601String("1Y");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // no time section designator
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1Y1H");
+    invalidISO = DurationUnits::fromISO8601String("P1Y1H");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // incorrect order in date section
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1M1Y");
+    invalidISO = DurationUnits::fromISO8601String("P1M1Y");
     ASSERT_TRUE(!invalidISO.has_value());
     // incorrect order in time section
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1YT1M1H");
+    invalidISO = DurationUnits::fromISO8601String("P1YT1M1H");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // number without designator
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1");
+    invalidISO = DurationUnits::fromISO8601String("P1");
     ASSERT_TRUE(!invalidISO.has_value());
 
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1YT1");
+    invalidISO = DurationUnits::fromISO8601String("P1YT1");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // designator without number
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("PY");
+    invalidISO = DurationUnits::fromISO8601String("PY");
     ASSERT_TRUE(!invalidISO.has_value());
 
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1YTH");
+    invalidISO = DurationUnits::fromISO8601String("P1YTH");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // trailing number
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1Y1");
+    invalidISO = DurationUnits::fromISO8601String("P1Y1");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // trailing designator
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1YM");
+    invalidISO = DurationUnits::fromISO8601String("P1YM");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // invalid designator in date section
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1Y1Z");
+    invalidISO = DurationUnits::fromISO8601String("P1Y1Z");
     ASSERT_TRUE(!invalidISO.has_value());
 
     // invalid designator in time section
-    invalidISO = hbt::mods::DurationUnits::fromISO8601String("P1Y1T1H1Z");
+    invalidISO = DurationUnits::fromISO8601String("P1Y1T1H1Z");
     ASSERT_TRUE(!invalidISO.has_value());
 }
 }; // namespace test::mods
