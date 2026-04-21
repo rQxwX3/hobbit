9e1e444d71eab6be6188b545973f1fd6dd274d04
rQxwX3 <github@scrt.simplelogin.com>
2026-04-21 09:49:54 +0300

    Implement mods::TaskOverride

---
+++ src/models/include/task_override.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <datetime.hpp>
+#include <recurrent_task.hpp>
+#include <task_data.hpp>
+
+#include <optional>
+
+namespace hbt::mods {
+class TaskOverride {
+  public:
+    using uuid_t = RecurrentTask::uuid_t;
+
+    using appliesOn_t = hbt::mods::DateTime;
+    using appliesUntil_t = std::optional<hbt::mods::DateTime>;
+
+    using title_t = std::optional<std::string>;
+    using deadline_t = std::optional<TaskData::deadline_t>;
+    using cancelled_t = std::optional<bool>;
+    using startFrom_t = std::optional<TaskData::startFrom_t>;
+
+    struct OverrideData {
+        title_t title_;
+        deadline_t deadline_;
+        cancelled_t cancelled_;
+        startFrom_t startFrom_;
+    };
+
+    enum class Priority : uint8_t {
+        SliceOverride,
+        IndividualOverride,
+    };
+
+  private:
+    uuid_t recurrentTaskUUID_;
+
+    hbt::mods::DateTime appliesOn_;
+    std::optional<hbt::mods::DateTime> appliesUntil_;
+
+    OverrideData override_;
+
+  private:
+    [[nodiscard]] auto validateUUID(const uuid_t &uuid) -> uuid_t;
+    [[nodiscard]] auto validateAppliesUntil(appliesUntil_t appliesUntil)
+        -> appliesUntil_t;
+
+  public:
+    TaskOverride(const uuid_t &recurrentTaskUUID, appliesOn_t appliesOn,
+                 appliesUntil_t appliesUntil, OverrideData override);
+};
+} // namespace hbt::mods
