0f22ab059deeafdfcbffa788379f454b43267910
rQxwX3 <github@scrt.simplelogin.com>
2026-05-03 12:02:27 +0300

    Finish implementing mods::YMD

--- src/models/include/ymd.hpp
+++ src/models/include/ymd.hpp
@@ -1,18 +1,39 @@
 #pragma once
 
+#include <duration.hpp>
+
+#include <cassert>
+#include <chrono>
 #include <cstdint>
+#include <expected>
 #include <string>
+#include <utility>
 
 namespace hbt::mods {
 class YMD {
   public:
-    using year_value_t = int16_t;
-    using month_value_t = uint8_t;
-    using day_value_t = uint8_t;
+    /* types below reflect value ranges for corresponding std::chrono types */
+    using year_t = int16_t;
+    using month_t = uint8_t;
+    using day_t = uint8_t;
+
+  public:
+    enum class weekday_t : uint8_t {
+        SUNDAY = 0,
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        COUNT_,
+    };
 
   public:
     enum class Error : uint8_t {
         InvalidArguments,
+
+        InvalidChrono,
     };
 
   public:
@@ -23,21 +44,51 @@ class YMD {
             return "YMD: cannot construct valid YMD object from provided "
                    "values";
 
+        case Error::InvalidChrono:
+            return "YMD: provided std::chrono::year_month_day value is not "
+                   "valid";
+
         default:
             std::unreachable();
         }
     }
 
   private:
-    year_value_t year_;
-    month_value_t month_;
-    day_value_t day_;
+    /* the order must not be changed */
+    year_t year_;
+    month_t month_;
+    day_t day_;
 
   public:
-    YMD(year_value_t year, month_value_t month, day_value_t day);
+    [[nodiscard]] auto toChrono() const -> std::chrono::year_month_day;
 
-  private:
-    auto validate() const -> void;
+    [[nodiscard]] static auto
+    fromChrono(const std::chrono::year_month_day &chrono)
+        -> std::expected<YMD, Error>;
+
+  public:
+    YMD(year_t year, month_t month, day_t day);
+
+  public:
+    [[nodiscard]] auto operator<=>(const YMD &other) const
+        -> std::strong_ordering;
+
+    [[nodiscard]] auto operator==(const YMD &other) const -> bool;
+
+  public:
+    [[nodiscard]] static auto today() -> YMD;
+
+  public:
+    [[nodiscard]] auto ok() const -> bool;
+
+  public:
+    [[nodiscard]] auto getYear() const -> year_t;
+
+    [[nodiscard]] auto getMonth() const -> month_t;
+
+    [[nodiscard]] auto getDay() const -> day_t;
+
+    [[nodiscard]] auto getWeekday() const -> weekday_t;
 };
 
 } // namespace hbt::mods
--- src/models/src/ymd.cpp
+++ src/models/src/ymd.cpp
@@ -1,20 +1,73 @@
 #include <ymd.hpp>
 
-#include <chrono>
+#include <cassert>
 
 namespace hbt::mods {
-auto YMD::validate() const -> void {
+[[nodiscard]] auto YMD::toChrono() const -> std::chrono::year_month_day {
     using namespace std::chrono;
 
     auto chrono{year_month_day(year{year_}, month{month_}, day{day_})};
 
+    return chrono;
+}
+
+[[nodiscard]] auto YMD::fromChrono(const std::chrono::year_month_day &chrono)
+    -> std::expected<YMD, Error> {
+    if (!chrono.ok()) {
+        return std::unexpected(Error::InvalidChrono);
+    }
+
+    return YMD(static_cast<year_t>(int(chrono.year())),
+               static_cast<month_t>(unsigned(chrono.month())),
+               static_cast<day_t>(unsigned(chrono.day())));
+}
+
+[[nodiscard]] auto YMD::ok() const -> bool {
+    auto chrono{toChrono()};
+
     if (!chrono.ok()) {
         throw std::invalid_argument(errorMessage(Error::InvalidArguments));
     }
+
+    return true;
 }
 
-YMD::YMD(year_value_t year, month_value_t month, day_value_t day)
+YMD::YMD(year_t year, month_t month, day_t day)
     : year_{year}, month_{month}, day_{day} {
-    validate();
+    assert(ok());
+}
+
+[[nodiscard]] auto YMD::operator<=>(const YMD &other) const
+    -> std::strong_ordering = default;
+
+[[nodiscard]] auto YMD::operator==(const YMD &other) const -> bool = default;
+
+auto YMD::today() -> YMD {
+    using namespace std::chrono;
+
+    auto now{system_clock::now()};
+    auto today{floor<days>(now)};
+    auto chrono{year_month_day(today)};
+
+    assert(chrono.ok());
+
+    auto ymdFromChrono{fromChrono(chrono)};
+    if (!ymdFromChrono) {
+        throw std::invalid_argument(errorMessage(ymdFromChrono.error()));
+    }
+
+    return ymdFromChrono.value();
+}
+
+[[nodiscard]] auto YMD::getYear() const -> year_t { return year_; }
+
+[[nodiscard]] auto YMD::getMonth() const -> month_t { return month_; }
+
+[[nodiscard]] auto YMD::getDay() const -> day_t { return day_; }
+
+[[nodiscard]] auto YMD::getWeekday() const -> weekday_t {
+    auto chrono{toChrono()};
+
+    return static_cast<weekday_t>(std::chrono::weekday{chrono}.c_encoding());
 }
 }; // namespace hbt::mods
