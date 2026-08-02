19c3c5455dbd7f33c8d12d2b91b4981aac8c6d01
rQxwX3 <github@scrt.simplelogin.com>
2026-08-02 16:36:04 -0700

    Add schema for rec::Recurrence

---
+++ src/calendar/include/recurrence/schema/recurrence.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <datetime/schema/datetime.hpp>
+#include <datetime/schema/opt_datetime.hpp>
+#include <recurrence/recurrence.hpp>
+#include <schema/schema.hpp>
+
+namespace clndr::rec::schema::recurrence {
+namespace fields {
+using namespace core::schema::fields;
+using StartDateTime =
+    Field<dt::DateTime, [](const rec::Recurrence &recurrence) -> dt::DateTime {
+        return recurrence.getStartDateTime();
+    }>;
+
+using EndDateTime =
+    Field<dt::OptDateTime,
+          [](const rec::Recurrence &recurrence) -> dt::OptDateTime {
+              return recurrence.getEndDateTime();
+          }>;
+
+using all = Fields<StartDateTime, EndDateTime>;
+}; // namespace fields
+
+namespace rules {
+using namespace core::schema::rules;
+using ValidStartDateTime = Rule<[](const rec::Recurrence &recurrence) -> bool {
+    return dt::schema::datetime::Schema::validate(
+        fields::StartDateTime::accessor(recurrence));
+},
+                                fields::StartDateTime>;
+
+using ValidEndDateTime = Rule<[](const rec::Recurrence &recurrence) -> bool {
+    return dt::schema::opt_datetime::Schema::validate(
+        fields::EndDateTime::accessor(recurrence));
+},
+                              fields::EndDateTime>;
+using ValidStartEndRelation =
+    Rule<[](const rec::Recurrence &recurrence) -> bool {
+        const auto startValue{fields::StartDateTime::accessor(recurrence)};
+        const auto endValue{fields::EndDateTime::accessor(recurrence)};
+
+        if (!endValue.hasValue()) {
+            return true;
+        }
+
+        return startValue < endValue.getValue();
+    },
+         fields::StartDateTime, fields::EndDateTime>;
+
+using all = Rules<ValidStartDateTime, ValidEndDateTime, ValidStartEndRelation>;
+}; // namespace rules
+
+using Schema = core::schema::Schema<rec::Recurrence, fields::all, rules::all>;
+}; // namespace clndr::rec::schema::recurrence
