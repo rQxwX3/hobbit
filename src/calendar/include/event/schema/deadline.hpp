9c9d5b878edd9fa4b6c25d113588c146f880b78b
rQxwX3 <github@scrt.simplelogin.com>
2026-08-02 14:11:30 -0700

    Add schema for ev::Deadline

---
+++ src/calendar/include/event/schema/deadline.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <event/deadline.hpp>
+#include <schema/fields.hpp>
+#include <schema/rules.hpp>
+#include <schema/schema.hpp>
+
+namespace clndr::ev::schema::deadline {
+namespace fields {
+using namespace core::schema::fields;
+
+using UnderlyingType =
+    Field<ev::Deadline::underlying_type_t,
+          [](const ev::Deadline &deadline) -> ev::Deadline::underlying_type_t {
+              return deadline.getUnderlyingType();
+          }>;
+
+using all = Fields<UnderlyingType>;
+}; // namespace fields
+
+namespace rules {
+using namespace core::schema::rules;
+
+using all = Rules<>;
+}; // namespace rules
+
+using Schema = core::schema::Schema<ev::Deadline, fields::all, rules::all>;
+}; // namespace clndr::ev::schema::deadline
