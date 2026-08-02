1f429e212ff7cc832ab5f76b82b1048c8c09b6fe
rQxwX3 <github@scrt.simplelogin.com>
2026-08-02 16:23:46 -0700

    Add schema for rec::IntervalPattern

---
+++ src/calendar/include/recurrence/schema/interval_pattern.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <datetime/schema/interval.hpp>
+#include <recurrence/interval_pattern.hpp>
+#include <schema/schema.hpp>
+
+namespace clndr::rec::schema::interval_pattern {
+namespace fields {
+using namespace core::schema::fields;
+using Interval =
+    Field<dt::Interval,
+          [](const rec::IntervalPattern &intervalPattern) -> dt::Interval {
+              return intervalPattern.getInterval();
+          }>;
+
+using all = Fields<Interval>;
+}; // namespace fields
+
+namespace rules {
+using namespace core::schema::rules;
+using ValidInterval =
+    Rule<[](const rec::IntervalPattern &intervalPattern) -> bool {
+        return dt::schema::interval::Schema::validate(
+            fields::Interval::accessor(intervalPattern));
+    },
+         fields::Interval>;
+
+using all = Rules<ValidInterval>;
+}; // namespace rules
+
+using Schema =
+    core::schema::Schema<rec::IntervalPattern, fields::all, rules::all>;
+}; // namespace clndr::rec::schema::interval_pattern
