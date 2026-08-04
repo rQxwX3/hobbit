fa2025ba97616c008eccafdc506cf5c5b9b14db6
rQxwX3 <github@scrt.simplelogin.com>
2026-08-04 13:49:12 -0700

    Update core::error to be constexpr

--- src/core/include/error.hpp
+++ src/core/include/error.hpp
@@ -1,24 +1,9 @@
 #pragma once
 
-#include <string>
+#include <fixed_string.hpp>
 
-namespace core::err {
-template <typename Derived> class Base {
-  public:
-    [[nodiscard]] static auto generateMessage(std::string_view message)
-        -> std::string {
-        return std::string(Derived::className) + ": " + std::string(message);
-    }
+namespace core::error {
+template <FixedString className, FixedString Message> struct Error {
+    static constexpr auto msg{className + FixedString{": "} + Message};
 };
-
-template <typename T>
-concept Concept = std::is_enum_v<typename T::Code> &&
-                  requires(typename T::Code code, std::string_view message) {
-                      typename T::Code;
-                      { T::className } -> std::convertible_to<std::string_view>;
-                      {
-                          T::generateMessage(message)
-                      } -> std::same_as<std::string>;
-                      { T::getMessage(code) } -> std::same_as<std::string>;
-                  };
-}; // namespace core::err
+}; // namespace core::error
