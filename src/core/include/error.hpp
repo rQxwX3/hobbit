2466be80d72d487725a1fb710156959bec9edcd8
rQxwX3 <github@scrt.simplelogin.com>
2026-08-04 20:21:02 -0700

    Update core::error to include code

--- src/core/include/error.hpp
+++ src/core/include/error.hpp
@@ -3,7 +3,10 @@
 #include <fixed_string.hpp>
 
 namespace core::error {
-template <FixedString className, FixedString Message> struct Error {
+using code_t = uint8_t;
+
+template <auto Code, FixedString className, FixedString Message> struct Error {
+    static constexpr auto code{code_t(Code)};
     static constexpr auto msg{className + FixedString{": "} + Message};
 };
 }; // namespace core::error
