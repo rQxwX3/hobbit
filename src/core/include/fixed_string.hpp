3c0d913564ef2ac3effaf3edf1c1df8584f37f92
rQxwX3 <github@scrt.simplelogin.com>
2026-08-05 21:36:28 -0700

    Add op+() to core::FixedString

--- src/core/include/fixed_string.hpp
+++ src/core/include/fixed_string.hpp
@@ -7,10 +7,28 @@ namespace core {
 template <std::size_t N> struct FixedString {
     char value[N];
 
+    constexpr FixedString() = default;
+
     constexpr FixedString(const char (&string)[N]) {
         std::copy_n(string, N, value);
     }
 
     constexpr operator std::string_view() const { return {value, N - 1}; }
+};
+
+template <std::size_t N1, std::size_t N2>
+constexpr auto operator+(const FixedString<N1> &lhs,
+                         const FixedString<N2> &rhs) {
+    FixedString<N1 + N2 - 1> result{};
+
+    for (std::size_t i = 0; i < N1 - 1; ++i) {
+        result.value[i] = lhs.value[i];
+    }
+
+    for (std::size_t i = 0; i < N2; ++i) {
+        result.value[N1 - 1 + i] = rhs.value[i];
+    }
+
+    return result;
 };
 }; // namespace core
