8e9aeeb5d509ad777632085f2fb8a6e232f92275
rQxwX3 <github@scrt.simplelogin.com>
2026-08-05 21:34:56 -0700

    Remove models directory

--- src/models/src/task/instance.cpp
+++
@@ -1,7 +0,0 @@
-#include <task/instance.hpp>
-
-namespace hbt::mods::task {
-Instance::Instance(uuid_t templateUUID, DateTime datetime, bool completed)
-    : uuid_{core::uuid::generateUUID()}, templateUUID_{std::move(templateUUID)},
-      datetime_{datetime}, completed_{completed} {}
-} // namespace hbt::mods::task
