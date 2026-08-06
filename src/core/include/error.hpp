3b26e7d07a23914ccbe196d784a81c616dac86ce
rQxwX3 <github@scrt.simplelogin.com>
2026-08-06 11:58:38 -0700

    Update core::error to inherit from std::exception

--- src/core/include/error.hpp
+++ src/core/include/error.hpp
@@ -5,8 +5,12 @@
 namespace core::error {
 using code_t = uint8_t;
 
-template <auto Code, FixedString className, FixedString Message> struct Error {
+template <auto Code, typename Base, FixedString className, FixedString Message>
+    requires std::derived_from<Base, std::exception>
+struct Error : Base {
     static constexpr auto code{code_t(Code)};
     static constexpr auto msg{className + FixedString{": "} + Message};
+
+    Error() : Base(std::string(msg)) {}
 };
 }; // namespace core::error
